86995b6d814a33feb779a87c134d2e29b37650b8
peytonkemp <peyton.kemp@gmail.com>
2021-04-29 14:56:51 -0500

    added modules and HTML for CarsRUs + interpolations

---
+++ scripts/CarsRUs.js
@@ -0,0 +1,39 @@
+import { Colors } from "./colors"
+import { Interiors } from "./interiors"
+import { Technologies } from "./technologies"
+import { Wheels } from "./wheels"
+
+
+export const CarsRUs = () => {
+    return `
+        <h1>Cars R Us</h1>
+
+        <article class="choices">
+            <section class="choices__colors options">
+                <h2>Paint Colors</h2>
+                ${Colors()}
+            </section>
+            <section class="choices__interiors options">
+                <h2>Interiors</h2>
+                ${Interiors()}
+            </section>
+            <section class="choices__technologies options">
+                <h2>Technologies</h2>
+                ${Technologies()}
+            </section>
+            <section class="choices__wheels options">
+                <h2>Wheels</h2>
+                ${Wheels()}
+            </section>
+        </article>
+
+        <article>
+            <button id="orderButton">Create Custom Order</button>
+        </article>
+
+        <article class="customOrders">
+            <h2>Custom Jewelry Orders</h2>
+            ${Orders()}
+        </article>
+    `
+}
